228c84b2e90cdc8abbbe31c13ce46ccab4ec05d2
Tyler Wright <tyler.wright@fullstackacademy.com>
2023-07-20 09:51:36 -0700

    initial commit

---
+++ Homes.js
@@ -0,0 +1,44 @@
+class Home {
+  constructor(numRooms, numBathrooms, squareFeet) {
+    this.numRooms = numRooms;
+    this.numBathrooms = numBathrooms;
+    this.squareFeet = squareFeet;
+  }
+
+  getHomeDetails() {
+    return `Rooms: ${this.numRooms}, Bathrooms ${this.numBathrooms}, Square Feet: ${this.squareFeet}`;
+  }
+}
+
+//sub class
+class SingleFamilyHome extends Home {
+  constructor(numRooms, numBathrooms, squareFeet, yardSize) {
+    super(numRooms, numBathrooms, squareFeet);
+    this.yardSize = yardSize;
+  }
+
+  getHomeDetails() {
+    return `${super.getHomeDetails()}, Yard Size: ${this.yardSize}`;
+  }
+}
+
+//sub class
+class Apartment extends Home {
+  constructor(numRooms, numBathrooms, squareFeet, floorNumber) {
+    super(numRooms, numBathrooms, squareFeet);
+    this.floorNumber = floorNumber;
+  }
+
+  getHomeDetails() {
+    return `${super.getHomeDetails()}, Floor Number: ${this.floorNumber}`;
+  }
+}
+
+const home = new Home(5, 2, 1400);
+console.log(home.getHomeDetails());
+
+const home2 = new SingleFamilyHome(3, 1, 1000, 500);
+console.log(home2.getHomeDetails());
+
+const home3 = new Apartment(2, 1, 800, 3);
+console.log(home3.getHomeDetails());
